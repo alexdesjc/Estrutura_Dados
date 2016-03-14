8ed703e4102bc985209575ec09d1a76d9d78355b
alexdesjc <alexsilvaro@gmail.com>
2016-03-14 17:29:13 -0300

    balanceamento com a pilha encadeada
    Entrega #5 Estrutura de Dados

--- balanceamento.py
+++ balanceamento.py
@@ -1,30 +1,83 @@
 import unittest
 
-
 class PilhaVaziaErro(Exception):
     pass
 
+
+class Noh():
+    def __init__(self, valor, direito=None, esquerdo=None):
+        '''
+        :param valor: valor do nó
+        :param direito: No direito
+        :param esquerdo: Nó esquerdo
+        Cria um objeto Noh
+        '''
+        self.direito = direito
+        self.esquerdo = esquerdo
+        self.valor = valor
+
+
 class Pilha():
+
     def __init__(self):
-        self._lista = []
+        '''
+        Cria uma Pilha
+        '''
+        self.tam = 0
+        self.primeiro = None
+        self.topo = None
 
-    def vazia(self):
-        return not bool(self._lista)
-
-    def topo(self):
-        if self._lista:
-            return self._lista[-1]
-        raise PilhaVaziaErro()
 
     def empilhar(self, valor):
-        self._lista.append(valor)
+        '''
+        :param valor: valor do Noh a ser adicionado
+        Adiciona um valor à Pilha
+        '''
+        noh = Noh(valor)
+        if self.tam == 0:
+            self.primeiro = noh
+            self.topo = noh
+        else:
+            ultimo = self.primeiro
+            while ultimo.direito is not None:
+                ultimo = ultimo.direito
+            noh.esquerdo = ultimo
+            ultimo.direito = noh
+            self.topo = noh
+
+        self.tam += 1
+
+    def _topo(self):
+        '''
+        :param valor: valor do Noh a ser adicionado
+        Adiciona um valor à Pilha
+        '''
+        ultimo = self.topo
+        return ultimo.valor
+
+    def vazia(self):
+        '''
+        Verifica se está vazia
+        '''
+        return not bool(self.tam)
 
     def desempilhar(self):
-        try:
-            return self._lista.pop()
-        except IndexError:
+        '''
+        Remove o último item da Pilha
+        '''
+        if self.tam == 0:
             raise PilhaVaziaErro()
 
+        ultimo = self.topo
+        if self.tam == 1:
+            self.topo = None
+            self.primeiro = None
+        else:
+            penultimo = ultimo.esquerdo
+            penultimo.direito = None
+            self.topo = penultimo
+        self.tam -= 1
+        return ultimo.valor
 
 def esta_balanceada(expressao):
     """
