8fce376d9824d39abab321f948c8af9a7d65dc09
alexdesjc <alexsilvaro@gmail.com>
2016-04-05 21:48:29 -0300

    QuickSort.py
    Entrega #9 Estrutura de Dados

---
+++ QuickSort.py
@@ -0,0 +1,57 @@
+import unittest
+def _quick_recursivo(seq, inicio, final):
+    if inicio >= final:
+        return seq
+    indice_pivot = final
+    pivot = seq[indice_pivot]
+    i_esquerda = inicio
+    i_direita = final - 1
+
+    #posicionando pivot
+    while i_esquerda <= i_direita:
+
+        while seq[i_direita] >= pivot and i_esquerda <= i_direita:
+            i_direita -= 1
+
+        while seq[i_esquerda] <= pivot and i_esquerda <= i_direita:
+            i_esquerda += 1
+
+        if i_direita > i_esquerda:
+            seq[i_direita], seq[i_esquerda] = seq[i_esquerda], seq[i_direita]
+
+    _fim = i_esquerda - 1
+    _inicio = i_esquerda + 1
+    seq[final], seq[i_esquerda] = seq[i_esquerda], seq[final]
+
+     # Resolvendo para sublista da esquerda
+    _quick_recursivo(seq, inicio, _fim)
+
+     # Resolvendo para sublista da direita
+    _quick_recursivo(seq, _inicio, final)
+
+    return seq
+
+
+def quick_sort(seq):
+    '''
+    Análise de Complexidade:
+    O quickSort roda em o de n ao quadrado em tempo de execução (no pior caso),
+    o de n * log de n no melhor caso e o de log de n em memória
+    '''
+    return _quick_recursivo(seq, 0, len(seq) - 1)
+
+def quick_sort(seq):
+    return _quick_recursivo(seq, 0, len(seq) - 1)
+
+class OrdenacaoTestes(unittest.TestCase):
+    def teste_lista_vazia(self):
+        self.assertListEqual([], quick_sort([]))
+
+    def teste_lista_unitaria(self):
+        self.assertListEqual([1], quick_sort([1]))
+
+    def teste_lista_binaria(self):
+        self.assertListEqual([1, 2], quick_sort([2, 1]))
+
+    def teste_lista_desordenada(self):
+        self.assertListEqual([0, 1, 2, 3, 4, 5, 6, 7, 8, 9], quick_sort([9, 7, 1, 8, 5, 3, 6, 4, 2, 0]))
